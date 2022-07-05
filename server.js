82e16c36fad3186b20bbbbb26791a475f65f6e39
jaykumar-bhatt <jaykumar.bhatt@bacancy.com>
2022-07-05 13:04:46 +0530

    add middelware to log request and parse request into json with limit 1mb

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const connectDb = require('./src/config/connection');
+const morgan = require('morgan')
 require('dotenv').config();
 const app = express();
 
-//database connection
-connectDb();
+const connectDb = require('./src/config/connection');
 
+const port = process.env.PORT || 8080;
 
+//handel cors error
 app.use(cors());
 
-app.listen(3000,()=> {
-  console.log('Server is running on 3000..')
-})
+//Mongodb connection
+connectDb();
 
+//log requests
+app.use(morgan('tiny'));
+
+//parse request into json with 1mb limit
+app.use(express.json({limit: '1mb'}));
+
+
+app.listen(port, ()=> {
+  console.log(`Server is running on port ${port}`)
+})
