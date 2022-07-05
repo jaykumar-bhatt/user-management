98ede19b30b8c56b24cc3bfc96edf2453441dcf8
jaykumar-bhatt <jaykumar.bhatt@bacancy.com>
2022-07-05 14:56:52 +0530

    add feature: login

--- src/controller/user/user.controller.js
+++ src/controller/user/user.controller.js
@@ -1,9 +1,8 @@
-const bcrypt = require('bcrypt');
-const users = require('../../model/user');
-const { createToken } = require('../../helper/index');
+const bcrypt = require("bcrypt");
+const users = require("../../model/user");
+const { createToken } = require("../../helper/index");
 
-exports.createUser = async (req,res) => {
-
+exports.createUser = async (req, res) => {
   const password = bcrypt.hashSync(req.body.password, 10);
   const { name, username, email, age } = req.body;
 
@@ -15,19 +14,49 @@ exports.createUser = async (req,res) => {
       password,
       age,
     };
-
     const newUser = await users.create(payload);
     const token = createToken(newUser);
 
-    res.status(201).json({
-      'message':'User created successfully.',
-      'Token':token
-    })
-
-  } catch(error) {
-    res.status(400).json({
-      'message':'Error while creating User.',
-      'error': error.message
-    })
+    return res.status(201).json({
+      message: "User created successfully.",
+      Token: token,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "Error while creating User.",
+      error: error.message,
+    });
   }
-}
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await users.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "Email not Found.",
+      });
+    }
+
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+      return res.status(403).json({
+        message:"password is incorrect.",
+      });
+    }
+
+    const token = createToken(user);
+    return res.status(201).json({
+      message: "User login successfully.",
+      Token: token,
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      message: "Error while login.",
+      error: error.message,
+    });
+  }
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,7 @@ const user = require('../controller/user/user.controller')
 const router = express.Router();
 
 // APIs
-router.post('/signin',user.createUser)
+router.post('/signin',user.createUser);
+router.post('/login',user.login);
 
 module.exports = router;
